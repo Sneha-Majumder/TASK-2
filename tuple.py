6c43a118f1986babb5b0f3aa6961799319dbe62e
Sneha-Majumder <64889474+Sneha-Majumder@users.noreply.github.com>
2020-05-11 17:43:11 +0530

    Create tuple.py
    Assigning elements from a tuple

---
+++ tuple.py
@@ -0,0 +1,9 @@
+Students = [(1804, "Asmi", 20), (1805, "Arav", 19), (1806, "Nirmalya", 22)]
+print("Printing the data");
+for i in Students;
+print(i);
+Students[0] = (1809, "Nakul", 23)
+print();
+print("Printing data after modification");
+for i in Students;
+print(i);
