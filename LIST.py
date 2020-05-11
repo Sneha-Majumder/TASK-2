b1d8b6c823a89d686eb4214b92cb41c2310542fd
Sneha-Majumder <64889474+Sneha-Majumder@users.noreply.github.com>
2020-05-11 17:16:45 +0530

    Create LIST.py
    code to assign elements to different lists

---
+++ LIST.py
@@ -0,0 +1,13 @@
+stu = ["ANNA", 1804, "India"]
+Dep1 = ["ETC", 202]
+Dep2 = ["CS", 205]
+HOD_CS = [4, "Mr. Rout"]
+HOD_ETC = [6, "Ms. Bindu"]
+print("printing student data...");
+print("Name: %s, ID: %d, Country: %s""(stu[1],stu[2],stu[3]));
+print("printing departments....");
+print("Department 1:\nName: %s, ID: %d\nName: %s,ID: %s"%(Dep1[0],Dep1[1],Dep[0],Dep[1])));
+print("HOD DETAILS.....");
+print("CS HOD NAME: %S, Id: %d"%(HOD_CS[1],HOD_CS[0]));
+print("ETC HOD NAME: %S, Id: %d"%(HOD_ETC[1],HOD_CETC[0]));
+print(type(stu),type(Dep1),type(Dep2),type(HOD_CS),type(HOD_ETC));
